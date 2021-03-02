3121a107e944af560eb05c831f4503d3097f2715
MKurtz666 <mkusy936@gmail.com>
2021-03-02 10:20:18 +0100

    added additional shape and rotation method

--- js/GameField.js
+++ js/GameField.js
@@ -17,7 +17,7 @@ export class GameField {
     }
 
     getShapes = () => {
-        return [Shapes.TRIANGLE, Shapes.SQUARE, Shapes.BAR, Shapes.STEP];
+        return [Shapes.TRIANGLE, Shapes.SQUARE, Shapes.BAR, Shapes.STEP, Shapes.EL];
     }
 
     setUpNewShape = () => {
@@ -28,7 +28,7 @@ export class GameField {
 
     getRandomShape = () => {
         const shapes = this.getShapes();
-        let index = Math.floor(Math.random() * 4);
+        let index = Math.floor(Math.random() * 5);
         let selectedShape = shapes[index];
         this.activateTiles(selectedShape.coordinates);
         return selectedShape;
@@ -90,6 +90,10 @@ export class GameField {
         }
     }
 
+    checkFullLines = () => {
+        return
+    }
+
     checkRotationAvailability = (coordinates) => {
         for (const coordinate of coordinates) {
             if (coordinate % 11 === 0 || coordinate > 220 || coordinate < 1 || this.tiles.get(coordinate).occupied && !this.tilesActive.includes(coordinate)) {
@@ -106,9 +110,11 @@ export class GameField {
             case "BAR":
                 this.rotateBar();
                 break
-            // case "STEP":
-            //     this.rotateStep();
-            //     break
+            case "STEP":
+                this.rotateStep();
+                break
+            case "EL":
+                this.rotateEl();
         }
     }
 
@@ -185,6 +191,86 @@ export class GameField {
                     activeTiles[0] += 10;
                     activeTiles[2] -= 10;
                     activeTiles[3] -= 20;
+                    this.currentShape.rotationState = Angles.DEFAULT;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+        }
+    }
+
+    rotateStep = () => {
+        let activeTiles = this.tilesActive;
+        let tileOne = activeTiles[0];
+        let tileTwo = activeTiles[2];
+        let tileThree = activeTiles[3];
+
+        switch (this.currentShape.rotationState) {
+            case Angles.DEFAULT:
+                if (this.checkRotationAvailability([tileOne - 10, tileTwo - 12, tileThree - 2])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] -= 10;
+                    activeTiles[2] -= 12;
+                    activeTiles[3] -= 2;
+                    this.currentShape.rotationState = Angles.ONEROTATION;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+            case Angles.ONEROTATION:
+                if (this.checkRotationAvailability([tileOne + 10, tileTwo + 12, tileThree + 2])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] += 10;
+                    activeTiles[2] += 12;
+                    activeTiles[3] += 2;
+                    this.currentShape.rotationState = Angles.DEFAULT;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+        }
+    }
+
+    rotateEl = () => {
+        let activeTiles = this.tilesActive;
+        let tileOne = activeTiles[0];
+        let tileTwo = activeTiles[1];
+        let tileThree = activeTiles[3];
+
+        switch (this.currentShape.rotationState) {
+            case Angles.DEFAULT:
+                if (this.checkRotationAvailability([tileOne + 2, tileTwo - 10, tileThree + 10])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] += 2;
+                    activeTiles[1] -= 10;
+                    activeTiles[3] += 10;
+                    this.currentShape.rotationState = Angles.ONEROTATION;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+            case Angles.ONEROTATION:
+                if (this.checkRotationAvailability([tileOne + 22, tileTwo + 12, tileThree - 12])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] += 22;
+                    activeTiles[1] += 12;
+                    activeTiles[3] -= 12;
+                    this.currentShape.rotationState = Angles.TWOROTATIONS;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+            case Angles.TWOROTATIONS:
+                if (this.checkRotationAvailability([tileOne - 2, tileTwo + 10, tileThree - 10])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] -= 2;
+                    activeTiles[1] += 10;
+                    activeTiles[3] -= 10;
+                    this.currentShape.rotationState = Angles.THREEROTATIONS;
+                    this.activateTiles(this.tilesActive);
+                    break
+                } else {break;}
+            case Angles.THREEROTATIONS:
+                if (this.checkRotationAvailability([tileOne - 22, tileTwo - 12, tileThree + 12])) {
+                    this.deactivateTiles(this.tilesActive);
+                    activeTiles[0] -= 22;
+                    activeTiles[1] -= 12;
+                    activeTiles[3] += 12;
                     this.currentShape.rotationState = Angles.DEFAULT;
                     this.activateTiles(this.tilesActive);
                     break
--- js/constants.js
+++ js/constants.js
@@ -9,6 +9,7 @@ export const Shapes = {
     TRIANGLE: {coordinates: [6, 16, 17, 18], name: "TRIANGLE"},
     SQUARE: {coordinates: [5, 6, 16, 17], name: "SQUARE"},
     BAR: {coordinates: [4, 5, 6, 7], name: "BAR"},
-    STEP: {coordinates: [5, 6, 17, 18], name: "STEP"}
+    STEP: {coordinates: [5, 6, 17, 18], name: "STEP"},
+    EL: {coordinates: [5, 16, 17, 18], name: "EL"}
 }
 
