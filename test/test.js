2ec459bef75c115d76daf97655384a6993413486
MKurtz666 <mkusy936@gmail.com>
2021-03-29 19:44:20 +0200

    committing the first succesful implementation of the test suite - only one test present

--- test/test.js
+++ test/test.js
@@ -1,10 +1,20 @@
+import {expect} from "chai";
+import jsdom from "jsdom";
+import {readFileSync} from "fs";
 import {GameField} from "../js/GameField.js";
 
+const {JSDOM} = jsdom;
+const htmlFileContents = readFileSync("index.html", "utf8");
 
-describe("model", function() {
+describe("Testing functionality of the model - GameField class:", function() {
+
+    beforeEach(() => {
+        const dom = new JSDOM(htmlFileContents);
+        global.document = dom.window.document;
+    });
     
-    it("has level", function() {
+    it("Instance has correct starting level", function() {
         let game = new GameField();
-        assert.equal(game.level, 1);
+        expect(game.level).to.equal(game.level, 1);
     });
 });
